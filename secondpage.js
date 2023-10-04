0fa956f7801cc1597bf8bd54585b3cc01fe31aed
LakshitaTak <134226199+LakshitaTak@users.noreply.github.com>
2023-10-04 11:36:32 +0530

    Update secondpage.js

--- secondpage.js
+++ secondpage.js
@@ -1,25 +1,36 @@
-const token = `e9e0a3d21eb728`
-window.onload = async() => {
+// const token=`e9e0a3d21eb728`
+window.onload = async () => {
     const urlParams = new URLSearchParams(window.location.search);
     const ip = urlParams.get('ip');
 
-    // Display IP
     document.getElementById('ip-add').innerHTML = `IP Address : <span>${ip}</span>`;
 
-    // Fetch and display location information
-    const response = await fetch(`https://ipinfo.io/${ip}/geo?token=${token}`);
-    const data = await response.json();
-    console.log(data)
-    getdataonui(data);
-
+    try {
+        const response = await fetch(`https://ipapi.co/${ip}/json/`);
+        const data = await response.json();
+        console.log(data)
+        getdataonui(data);
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Error fetching IP information: ' + error.message);
+    }
+   
 };
-
-const info = document.querySelector('.info');
-
-function getdataonui(data) {
-    const [lat, long] = data.loc.split(',');
-    const hostname = data ? .hostname || "N/A";
-    info.innerHTML = `
+function getCurrentTimeForTimezone(timezone) {
+    const date = new Date();
+    const utcDate = new Date(date.toLocaleString('en-US', { timeZone: 'UTC' }));
+    const tzDate = new Date(date.toLocaleString('en-US', { timeZone: timezone }));
+    const offset = (tzDate - utcDate) / (60 * 60 * 1000);
+    const hours = date.getUTCHours() + offset;
+    date.setHours(hours);
+    return date;
+}
+const info=document.querySelector('.info');
+function getdataonui(data){
+    const lat = data.latitude;
+    const long = data.longitude;
+    const hostname = data?.hostname || "N/A";
+    info.innerHTML=`
     <div class="info-div">
     <p>Lat: <span>${lat}</span></p>
     <p>Long: <span>${long}</span></p>
@@ -30,46 +41,46 @@ function getdataonui(data) {
         </div>
         <div class="info-div">
             <p>Organisation: <span>${data.org}</span></p>
-            <p>Hostname: <span>${hostname}</span></p>
+            <p>Hostname: <span>${data.asn}</span></p>
         </div> `;
-    const mapIframe = document.getElementById('location-map');
-    mapIframe.src = `https://maps.google.com/maps?q=${lat},${long}&z=15&output=embed`;
+        const mapIframe = document.getElementById('location-map');
+        mapIframe.src = `https://maps.google.com/maps?q=${lat},${long}&z=15&output=embed`;
 
-    document.getElementById('time-zone').innerText = data.timezone;
-    const currentDateTime = new Date().toLocaleString("en-US", { timeZone: data.timezone });
-    document.getElementById('date-time').innerText = currentDateTime;
-
-    document.getElementById('pincode').innerText = data.postal;
-    if (data.postal) {
-        document.getElementById('message').innerText = "1";
-        getPostOffices(data.postal);
-    } else {
-        document.getElementById('message').innerText = "0";
-    }
-
+        document.getElementById('time-zone').innerText = data.timezone;
+        const currentTime = getCurrentTimeForTimezone(data.timezone);
+        document.getElementById('date-time').innerText = currentTime;
+    
+        document.getElementById('pincode').innerText = data.postal;
+        if(data.postal) {
+           
+            getPostOffices(data.postal);
+        } else {
+            document.getElementById('message').innerText = "0";
+        }
+    
 }
 async function getPostOffices(pincode) {
     const response = await fetch(`https://api.postalpincode.in/pincode/${pincode}`);
     const data = await response.json();
-
+    
     if (data[0].Status === 'Success') {
         const postOffices = data[0].PostOffice;
         console.log(postOffices)
+        document.getElementById('message').innerText = `${postOffices.length}`;
         displayPostOffices(postOffices);
-        filterPostOffices(postOffices);
+       filterPostOffices(postOffices);
     } else {
         alert('No post offices found for this pincode.');
     }
 }
-
 function displayPostOffices(postOffices) {
     const container = document.querySelector('.listing-container');
     container.innerHTML = ""; // clear existing entries
 
     postOffices.forEach(office => {
         const card = document.createElement('div');
-        card.className = 'card';
-        card.innerHTML = `
+        card.className='card';
+        card.innerHTML=`
                     <p> ${office.Name}</p>
                     <p>${office.BranchType}</p>
                     <p> ${office.DeliveryStatus}</p>
@@ -79,10 +90,9 @@ function displayPostOffices(postOffices) {
         container.appendChild(card);
     });
 }
-
 function filterPostOffices(postOffices) {
     const searchInput = document.getElementById('post-office-search');
-
+    
     searchInput.addEventListener('input', function() {
         const query = this.value.toLowerCase();
         const filtered = postOffices.filter(office => office.Name.toLowerCase().includes(query) || office.BranchType.toLowerCase().includes(query));
